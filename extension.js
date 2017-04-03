8bd39478216a06c1053d5541e21d40de460e0f3f
yzane <yzane@outlook.jp>
2017-04-04 00:56:15 +0900

    Add: markdown-pdf.convertOnSaveExclude option #16 Excluded file name of convertOnSave option

--- extension.js
+++ extension.js
@@ -90,7 +90,27 @@ function MarkdownPdfOnSave () {
   if (mode != 'markdown') {
     return;
   }
-  MarkdownPdf();
+  if (!isMarkdownPdfOnSaveExclude()) {
+    MarkdownPdf();
+  }
+}
+
+function isMarkdownPdfOnSaveExclude () {
+  var editor = vscode.window.activeTextEditor;
+  var filename = path.basename(editor.document.fileName);
+  var patterns = vscode.workspace.getConfiguration('markdown-pdf')['convertOnSaveExclude'] || '';
+  var pattern;
+  var i;
+  if (patterns && Array.isArray(patterns) && patterns.length > 0) {
+    for (i = 0; i < patterns.length; i++) {
+      pattern = patterns[i];
+      var re = new RegExp(pattern);
+      if (re.test(filename)) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /*
