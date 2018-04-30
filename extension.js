0bfc3cad1c6922c97aa0504a7a899e63a02ed3f0
yzane <yzane@outlook.jp>
2018-04-30 17:31:57 +0900

    Clean

--- extension.js
+++ extension.js
@@ -651,24 +651,29 @@ function fixHref(resource, href) {
   if (!href) {
     return href;
   }
+
   // Use href if it is already an URL
   const hrefUri = vscode.Uri.parse(href);
   if (['http', 'https'].indexOf(hrefUri.scheme) >= 0) {
     return hrefUri.toString();
   }
+
   // Use a home directory relative path If it starts with ^.
   if (href.indexOf('~') === 0) {
     return vscode.Uri.file(href.replace(/^~/, os.homedir())).toString();
   }
+
   // Use href as file URI if it is absolute
   if (path.isAbsolute(href) || hrefUri.scheme === 'file') {
     return vscode.Uri.file(href).toString();
   }
+
   // Use a workspace relative path if there is a workspace
   let root = vscode.workspace.getWorkspaceFolder(resource);
   if (root) {
     return vscode.Uri.file(path.join(root.uri.fsPath, href)).toString();
   }
+
   // Otherwise look relative to the markdown file
   return vscode.Uri.file(path.join(path.dirname(resource.fsPath), href)).toString();
 }
