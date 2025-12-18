bfdf3b326179a6eb458006f8f4edbc2a83290651
Juuso Turunen <44379448+juuso-turunen@users.noreply.github.com>
2025-12-18 14:30:35 +0200

    Add setting to set preferred language for the Puppeteer

--- extension.js
+++ extension.js
@@ -398,7 +398,7 @@ function exportPdf(data, filename, type, uri) {
         exportHtml(data, tmpfilename);
         var options = {
           executablePath: vscode.workspace.getConfiguration('markdown-pdf')['executablePath'] || puppeteer.executablePath(),
-          args: ['--lang='+vscode.env.language, '--no-sandbox', '--disable-setuid-sandbox']
+          args: ['--lang=' + vscode.workspace.getConfiguration('markdown-pdf')['preferredLanguage'] || vscode.env.language, '--no-sandbox', '--disable-setuid-sandbox']
           // Setting Up Chrome Linux Sandbox
           // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
       };
