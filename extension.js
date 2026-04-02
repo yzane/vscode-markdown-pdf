a1952a5c24c674d9b8fc37b0d0443ad6a250e7a4
yzane <yzane@outlook.jp>
2026-04-03 02:34:11 +0900

    fix: support include paths without leading space

--- extension.js
+++ extension.js
@@ -214,7 +214,7 @@ function convertMarkdownToHtml(filename, type, text) {
   if (vscode.workspace.getConfiguration('markdown-pdf')['markdown-it-include']['enable']) {
     md.use(require("markdown-it-include"), {
       root: path.dirname(filename),
-      includeRe: /:\[.+\]\((.+\..+)\)/i,
+      includeRe: /:\[.+\](\(.+\..+\))/i,
       bracesAreOptional: true
     });
   }
