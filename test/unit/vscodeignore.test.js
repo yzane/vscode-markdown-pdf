a092d132d5db8a2e19b62639bf6f1a11a330579b
yzane <yzane@outlook.jp>
2026-04-04 17:26:07 +0900

    chore: strengthen .vscodeignore for esbuild bundling

--- test/unit/vscodeignore.test.js
+++ test/unit/vscodeignore.test.js
@@ -6,10 +6,30 @@ var fs = require('fs');
 var path = require('path');
 
 describe('.vscodeignore', function () {
-  it('should exclude the unused emoji-images json payload from packaging', function () {
-    var vscodeignorePath = path.join(__dirname, '..', '..', '.vscodeignore');
-    var vscodeignore = fs.readFileSync(vscodeignorePath, 'utf-8');
+  var vscodeignore;
 
-    assert.match(vscodeignore, /^node_modules\/emoji-images\/json\/\*\*$/m);
+  it('should load .vscodeignore', function () {
+    var vscodeignorePath = path.join(__dirname, '..', '..', '.vscodeignore');
+    vscodeignore = fs.readFileSync(vscodeignorePath, 'utf-8');
+    assert.ok(vscodeignore.length > 0);
+  });
+
+  it('should exclude all node_modules by default', function () {
+    assert.match(vscodeignore, /^node_modules\/\*\*$/m);
+  });
+
+  it('should exclude source files that are bundled', function () {
+    assert.match(vscodeignore, /^extension\.js$/m);
+    assert.match(vscodeignore, /^src\/\*\*$/m);
+  });
+
+  it('should re-include puppeteer-core and its dependencies', function () {
+    assert.match(vscodeignore, /^!node_modules\/puppeteer-core\/\*\*$/m);
+    assert.match(vscodeignore, /^!node_modules\/@puppeteer\/browsers\/\*\*$/m);
+  });
+
+  it('should re-include runtime asset packages', function () {
+    assert.match(vscodeignore, /^!node_modules\/emoji-images\/pngs\/\*\*$/m);
+    assert.match(vscodeignore, /^!node_modules\/highlight\.js\/styles\/\*\*$/m);
   });
 });
